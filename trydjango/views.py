c4f0aa6a8529441f3d767826ce63796d5ac109f1
Maksym Shvets <maxshvets1901@gmail.com>
2021-10-21 17:49:25 +0300

    Listing Data in Views & Templates

--- trydjango/views.py
+++ trydjango/views.py
@@ -8,8 +8,10 @@ def home_view(request):
     name = "Justin"
     random_id = random.randint(1, 2)
     article_obj = Article.objects.get(id=random_id)
+    article_queryset = Article.objects.all()
 
     context = {
+        'object_list': article_queryset,
         'title': article_obj.title,
         'id': article_obj.id,
         'content': article_obj.content
