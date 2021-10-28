08af086f931412955ae9859f98e0cc93808da005
Maksym Shvets <maxshvets1901@gmail.com>
2021-10-28 15:33:37 +0300

    Basic HTML Form

--- articles/views.py
+++ articles/views.py
@@ -12,11 +12,22 @@ def article_search_view(request):
 
     article_obj = None
     if query:
-        article_obj = Article.objects.get (id=query)
+        article_obj = Article.objects.get(id=query)
     context = {
         "object": article_obj,
     }
     return render(request, "articles/search.html", context=context)
+
+
+def article_create_view(request):
+    context = {}
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        article_object = Article.objects.create(title=title, content=content)
+        context["object"] = article_object
+        context["created"] = True
+    return render(request, "articles/create.html", context=context)
 
 
 def article_detail_view(request, id=None):
