0bb8675986ec20e153525a4284c3109bfad980be
Maksym Shvets <maxshvets1901@gmail.com>
2021-11-02 19:22:57 +0200

    Basic Django Form

---
+++ articles/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class ArticleForm(forms.Form):
+    title = forms.CharField()
+    content = forms.CharField()
+
+    # def clean_title(self):
+    #     cleaned_data = self.cleaned_data  # dict
+    #     title = cleaned_data.get("title")
+    #     return title
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        title = cleaned_data.get("title")
+        content = cleaned_data.get("content")
+        return cleaned_data
